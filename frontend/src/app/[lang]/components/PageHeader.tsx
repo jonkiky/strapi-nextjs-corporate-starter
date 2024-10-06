f60a6fd6784d58b83009c196544663c6b77b28c7
Chen <jonkiky@gmail.com>
2024-10-06 14:04:12 -0400

    update the page style

--- frontend/src/app/[lang]/components/PageHeader.tsx
+++ frontend/src/app/[lang]/components/PageHeader.tsx
@@ -5,7 +5,7 @@ interface PageHeaderProps {
 
 export default function PageHeader({ heading, text } : PageHeaderProps) {
   return (
-    <div className="my-16 w-full text-center">
+    <div className="p-4  container flex mx-auto ">
     { text && <span className="text-violet-400 font-bold">{text}</span> }
     <h2 className="text-4xl my-4 lg:text-5xl font-bold font-heading">{heading}</h2>
   </div>
--- frontend/src/app/[lang]/page.tsx
+++ frontend/src/app/[lang]/page.tsx
@@ -1,23 +1,89 @@
-import LangRedirect from './components/LangRedirect';
-import {sectionRenderer} from './utils/section-renderer';
-import {getPageBySlug} from "@/app/[lang]/utils/get-page-by-slug";
+"use client";
+import { useState, useEffect, useCallback } from "react";
+import { fetchAPI } from "./utils/fetch-api";
 
+import Loader from "./components/Loader";
+import Blog from "./views/blog-list";
+import PageHeader from "./components/PageHeader";
 
-export default async function RootRoute({params}: { params: { lang: string } }) {
+interface Meta {
+  pagination: {
+    start: number;
+    limit: number;
+    total: number;
+  };
+}
+
+export default function Profile() {
+  const [meta, setMeta] = useState<Meta | undefined>();
+  const [data, setData] = useState<any>([]);
+  const [isLoading, setLoading] = useState(true);
+
+  const fetchData = useCallback(async (start: number, limit: number) => {
+    setLoading(true);
     try {
-      const page = await getPageBySlug('home', params.lang)
-      if (page.error && page.error.status == 401)
-        throw new Error(
-          'Missing or invalid credentials. Have you created an access token using the Strapi admin panel? http://localhost:1337/admin/'
-        )
+      const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
+      const path = `/articles`;
+      const urlParamsObject = {
+        sort: { createdAt: "desc" },
+        populate: {
+          cover: { fields: ["url"] },
+          category: { populate: "*" },
+          authorsBio: {
+            populate: "*",
+          },
+        },
+        pagination: {
+          start: start,
+          limit: limit,
+        },
+      };
+      const options = { headers: { Authorization: `Bearer ${token}` } };
+      const responseData = await fetchAPI(path, urlParamsObject, options);
 
-      if (page.data.length == 0 && params.lang !== 'en') return <LangRedirect />
-      if (page.data.length === 0) return null
-      const contentSections = page.data[0].attributes.contentSections
-      return contentSections.map((section: any, index: number) =>
-        sectionRenderer(section, index)
-      )
-    } catch (error: any) {
-      window.alert('Missing or invalid credentials')
+      if (start === 0) {
+        setData(responseData.data);
+      } else {
+        setData((prevData: any[] ) => [...prevData, ...responseData.data]);
+      }
+
+      setMeta(responseData.meta);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
+  }, []);
+
+  function loadMorePosts(): void {
+    const nextPosts = meta!.pagination.start + meta!.pagination.limit;
+    fetchData(nextPosts, Number(process.env.NEXT_PUBLIC_PAGE_LIMIT));
+  }
+
+  useEffect(() => {
+    fetchData(0, Number(process.env.NEXT_PUBLIC_PAGE_LIMIT));
+  }, [fetchData]);
+
+  if (isLoading) return <Loader />;
+
+  return (
+    <div>
+      <PageHeader heading=""  />
+      <Blog data={data}>
+        {meta!.pagination.start + meta!.pagination.limit <
+          meta!.pagination.total && (
+          <div className="flex justify-center">
+           <button
+            type="button"
+            className="px-6 py-3 text-sm rounded-lg hover:underline bg-pink-500 text-white hover:bg-pink-600"
+            onClick={loadMorePosts}
+          >
+            Load more posts
+          </button>
+
+          </div>
+        )}
+      </Blog>
+    </div>
+  );
 }
